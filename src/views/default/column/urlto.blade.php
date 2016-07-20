3a113d9af45edd3103f4377e62a2a3ae175089ee
Gennadiy <gennadiy_bilyk@mail.ru>
2016-07-20 14:14:14 +0300

    Правка по td

--- src/views/default/column/urlto.blade.php
+++ src/views/default/column/urlto.blade.php
@@ -1,9 +1,9 @@
+<td>
 @if ( ! is_null($instance))
-    <td>
     <a href="{{ $instance['href']  }}"
     @foreach($attributes as $key=>$value)
     {{ $key }}="{{   $value }}"
     @endforeach
     >{{ $instance['text'] }}</a>
-    </td>
 @endif
+</td>
