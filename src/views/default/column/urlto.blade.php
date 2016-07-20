1d0f42258dfbd9a088ca70d1b30cd4da9dde1584
Gennadiy <gennadiy_bilyk@mail.ru>
2016-07-20 14:12:09 +0300

    ДОбавил td так как делаю women-rules.ru и там баги вывод без td каряво

--- src/views/default/column/urlto.blade.php
+++ src/views/default/column/urlto.blade.php
@@ -1,7 +1,9 @@
 @if ( ! is_null($instance))
+    <td>
     <a href="{{ $instance['href']  }}"
     @foreach($attributes as $key=>$value)
     {{ $key }}="{{   $value }}"
     @endforeach
     >{{ $instance['text'] }}</a>
+    </td>
 @endif
